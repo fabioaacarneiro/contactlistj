557c891b0a8951c52e09f2f6086d5fb7a7307e26
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-03-27 11:50:12 -0300

    update input of form for new contacts

--- friend-list-j-client/src/components/Molecules/ContactForm/index.tsx
+++ friend-list-j-client/src/components/Molecules/ContactForm/index.tsx
@@ -31,9 +31,9 @@ export function ContactForm() {
     return (
         <div className="contact-form">
             <form onSubmit={evt => createContact(evt)}>
-                <input type="text" value={contactName} onChange={evt => setContactName(evt.target.value)} />
-                <input type="text" value={contactPhone} onChange={evt => setContactPhone(evt.target.value)} />
-                <input type="text" value={contactAddress} onChange={evt => setContactAddress(evt.target.value)} />
+                <input type="text" value={contactName} onChange={evt => setContactName(evt.target.value)} placeholder="Qual o nome do Contato?" />
+                <input type="text" value={contactPhone} onChange={evt => setContactPhone(evt.target.value)} placeholder="Qual o número de telefone do contato ?" />
+                <input type="text" value={contactAddress} onChange={evt => setContactAddress(evt.target.value)} placeholder="Qual o endereço do contato ?" />
                 <input className="sendButton" type="submit" value="Enviar" />
             </form>
         </div>
